921041d6bd2b5feb877bbafbd875f91e9bbfa102
Saygan4ik <sergey.saygan@gmail.com>
2018-01-26 17:05:32 +0600

    change mailers for sendgrid

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'sidekiq'
 gem 'sidekiq-scheduler'
 gem 'rack-attack'
 gem 'rack-cors', require: 'rack/cors'
+gem 'sendgrid-ruby'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,27 @@
 # frozen_string_literal: true
 
+include SendGrid
+
 class UserMailer < ApplicationMailer
   def welcome_email(user)
     mail(to: user.email, subject: 'Welcome to Pikabu-clone')
+
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    puts response.headers
   end
 
   def notification_if_post_set_hot(post)
     @url = '' + root_path + 'posts/' + post.id
     mail(to: post.user.email,
          subject: 'Your post has received the status of hot')
+
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    puts response.headers
   end
 end
