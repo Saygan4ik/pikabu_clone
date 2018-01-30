67ae3af4a38a24c241125d59b9df4b6acca24661
Saygan4ik <sergey.saygan@gmail.com>
2018-01-30 15:20:36 +0600

    change mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,10 +84,11 @@ Rails.application.configure do
   # Setup the mailer config
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'pikabu_clone.com',
+    :domain => 'desolate-hamlet-99998.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
